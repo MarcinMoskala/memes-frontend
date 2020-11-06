0d8d1399161f1f612770e4cfa1d3e3b52bdc7400
Marcin Moskala <marcin.moskala@allegro.pl>
2020-11-06 11:41:11 +0100

    Upload images

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import {useForm} from "react-hook-form";
 import {useWebSocket} from "react-use-websocket/dist/lib/use-websocket";
 import {ReadyState} from "react-use-websocket/dist";
 
+const baseUrl = "localhost:8080"
+
 function App() {
     const [memes, setMemes] = useState<Memes>();
-    const {sendMessage, lastMessage, readyState,} = useWebSocket("ws://localhost:8080/ws");
+    const [image, setImage] = useState<string>();
+    const {sendMessage, lastMessage, readyState,} = useWebSocket(`ws://${baseUrl}/ws`);
     const {register, handleSubmit, errors} = useForm<Meme>();
 
     useEffect(() => {
@@ -17,8 +20,30 @@ function App() {
     }, [lastMessage])
 
     const sendMeme = (meme: Meme) => {
-        console.log(`Sending ${JSON.stringify(meme)}`)
-        sendMessage(JSON.stringify(meme))
+        fetch(`http://${baseUrl}/meme/imgSrc`, {
+            method: "POST",
+            headers: {"Content-Type": "application/json",},
+            body: JSON.stringify({
+                ...meme,
+                ...(image ? {imgBase64: image} : {})
+            })
+        })
+    }
+
+    const deleteMeme = (memeId: string) => {
+        fetch(`http://${baseUrl}/meme/${memeId}`, {
+            method: "DELETE",
+            headers: {"Content-Type": "application/json",}
+        })
+    }
+
+    const addImage = (event: any) => {
+        let file = event.target.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            setImage(reader.result as string)
+        }
+        reader.readAsDataURL(file)
     }
 
     return <div className="App">
@@ -26,12 +51,16 @@ function App() {
 
         {memes && memes.memes && memes.memes.map(meme => <div>
             {meme.title && <h1>{meme.title}</h1>}
-            <img alt="meme" src={meme.imgSrc ?? "https://image.shutterstock.com/image-vector/no-sign-isolated-on-white-260nw-323390270.jpg"}/>
+            <img alt="meme"
+                 src={meme.imgBase64 ?? meme.imgSrc ?? "https://image.shutterstock.com/image-vector/no-sign-isolated-on-white-260nw-323390270.jpg"}/>
             {meme.text && <div>{meme.text}</div>}
-            <div style={{marginBottom: "40px"}}>{"Author: " + meme.author}</div>
+            <div>{"Author: " + meme.author}</div>
+            <div onClick={() => deleteMeme(meme.id)}>Delete</div>
+            <div style={{marginBottom: "40px"}}/>
         </div>)}
 
         <form onSubmit={handleSubmit(sendMeme)}>
+            {image && <img src={image}/>}
             <fieldset>
                 <div>
                     <label htmlFor="author">Autor</label>
@@ -39,11 +68,15 @@ function App() {
                     {errors.author && errors.author.message}
                 </div>
 
+                <input type="file" onChange={addImage}/>
+
+                {!image &&
                 <div>
                     <label htmlFor="imgSrc">Img src</label>
                     <input type="text" name="imgSrc" id="imgSrc" ref={register({required: "Wymagane"})}/>
                     {errors.imgSrc && errors.imgSrc.message}
                 </div>
+                }
 
                 <div>
                     <label htmlFor="text">Komentarz</label>
@@ -66,8 +99,19 @@ type Memes = {
 }
 
 type Meme = {
+    id: string,
     author: string,
     title: string | null,
+    imgBase64: string | null,
     text: string | null,
     imgSrc: string | null,
 }
+
+const urlToData = (url: string) => fetch(url)
+    .then(response => response.blob())
+    .then(blob => new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onloadend = () => resolve(reader.result)
+        reader.onerror = reject
+        reader.readAsDataURL(blob)
+    }))
